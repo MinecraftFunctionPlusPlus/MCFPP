bcbc0076182c38b0a9ca4b0dcf02bcecf8d27d5c
XiLaiTL <1272720210@qq.com>
2024-02-07 11:00:17 +0800

    feat(*) change type test

--- src/main/kotlin/top/mcfpp/lang/type/MCFPPBaseType.kt
+++ src/main/kotlin/top/mcfpp/lang/type/MCFPPBaseType.kt
@@ -4,12 +4,13 @@ package top.mcfpp.lang.type
  * 类型单例
  */
 enum class MCFPPBaseType(
-    override var typeName:kotlin.String
+    override var typeName:kotlin.String,
+    override var parentType: List<MCFPPType>
 ):MCFPPType {
-    Int("int"),
-    String("string"),
-    Float("float"),
-    Bool("bool"),
-    Any("any")
+    Any("any", listOf(Any)),
+    Int("int",listOf(Any)),
+    String("string",listOf(Any)),
+    Float("float",listOf(Any)),
+    Bool("bool",listOf(Any)),
 }
 
